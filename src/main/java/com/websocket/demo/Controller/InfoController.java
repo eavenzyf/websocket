b4b40ea346e8a3dd60ef8ab0a3d7f965873f8e66
administrator <administrator>
2019-07-10 16:52:59 +0800

    Initial commit

---
+++ src/main/java/com/websocket/demo/Controller/InfoController.java
@@ -0,0 +1,31 @@
+package com.websocket.demo.Controller;
+
+import com.websocket.demo.WebSocket.WebSocketServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @Classname InfoController
+ * @Description TODO
+ * @Date 2019/7/10 15:05
+ * @Created by du
+ */
+@RestController
+public class InfoController {
+    @Autowired
+    WebSocketServer server;
+    @PostMapping("login")
+    public String login(String username,String password) throws Exception{
+        server.sendInfo(username + "进入了聊天室!");
+        return username;
+    }
+    @RequestMapping("")
+    public ModelAndView showIndex(){
+        System.out.println("controller");
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+}
---
+++ src/main/java/com/websocket/demo/WebSocket/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.websocket.demo.WebSocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @Classname WebSocketConfig
+ * @Description TODO
+ * @Date 2019/7/10 14:53
+ * @Created by du
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
